1c871569adafae1c8f3416b0808d49094a9d4f33
Educated Savage <educatedsavage@educatedsavage.com>
2022-07-28 17:03:00 -0700

    Filling out server.js and adding index.ejs

--- server.js
+++ server.js
@@ -7,17 +7,33 @@ const MongoClient = require('mongodb').MongoClient
 MongoClient.connect('mongodb+srv://EducatedSavage:rYT9jdbEp5meqbhB@cluster0.nhpo8yc.mongodb.net/test?retryWrites=true&w=majority', (err, client) => {
     if (err) return console.error(err);
     console.log("Connected to Database");
+    const db = client.db('my-todos');
+    const todoList = db.collection('todos');
+    app.listen(3000, function() {
+        console.log('listening on 3000');
+    })
+    app.set('view engine', 'ejs')
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/index.html');
+    })
+    app.post('/todos', (req, res) => {
+        todoList.insertOne(req.body)
+            .then(result => {
+                res.redirect('/');
+            })
+            .catch(error => console.error(error))        
+    })
+    app.get('/', (req, res) => {
+        db.collection('todos').find().toArray()
+        .then(results => {
+            res.render('index.ejs', { todos: results })
+        })
+        .catch(error => console.error(error));
+        
+    })
 })
 
-app.listen(3000, function() {
-    console.log('listening on 3000');
-})
-app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
-app.post('/todos', (req, res) => {
-    console.log(req.body);
-})
 
-//TODO left off on the "Changing the Database" step on https://zellwk.com/blog/crud-express-mongodb/ after connecting to the MongoDatabase
+
+//TODO left off on the "Using EJS" step on https://zellwk.com/blog/crud-express-mongodb/ where it says "we see lots of [object Objest] because..."
